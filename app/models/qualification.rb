5c17b318665cc8c7aed3a6c36271700cd30bd782
kevin bett <kevin@rebelcolony.com>
2010-04-21 09:04:10 +0600

    changed image sizes, form css twees, and validations for file uploads

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -5,4 +5,6 @@ class Qualification < ActiveRecord::Base
   belongs_to :technique
   belongs_to :level
   has_attached_file :cert, :styles => { :medium => "800x800>", :thumb => "50x50>" }
+  validates_attachment_content_type :cert, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+  validates_attachment_size :cert, :less_than => 1.megabyte
 end

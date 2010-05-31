8f4435551cc21fab1f45b2288bedf806e684cf92
Alastair Brunton <info@simplyexcited.co.uk>
2010-05-31 22:18:27 +0200

    Working on writing tests for the search

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -7,6 +7,25 @@ describe SearchesController do
       get :index
       assert_response :success, @response.body
     end
+    
+    
+    it "should find all users with an ALL / ALL search" do
+      
+    end
+    
+    it "should find users in a certain body" do
+    
+    
+    end
+    
+    it "should find users with a certain technique" do
+      
+    end
+    
+    it "should find users with a technique and a body" do
+      
+    end
+    
   end
   
 end
---
+++ test/factories.rb
@@ -0,0 +1,17 @@
+
+
+Factory.define :body do |b|
+  b.name 
+end
+
+Factory.define :technique do |t|
+  
+end
+
+Factory.define :qualification do |t|
+  
+end
+
+Factory.define :user do |u|
+  
+end
